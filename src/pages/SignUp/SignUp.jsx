ed62916011d14d643eca3f5a62d97860fe51e0d5
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-01-02 09:07:43 +0600

    conceptual part 3 done

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,6 +3,8 @@ import { FcGoogle } from 'react-icons/fc'
 import useAuth from '../../hooks/useAuth'
 import { toast } from 'react-hot-toast'
 import { TbFidgetSpinner } from 'react-icons/tb'
+import axios from 'axios'
+import { imageUpload } from '../../api/utils'
 
 const SignUp = () => {
   const { createUser, updateUserProfile, signInWithGoogle, loading } = useAuth()
@@ -14,16 +16,26 @@ const SignUp = () => {
     const name = form.name.value
     const email = form.email.value
     const password = form.password.value
+    const image = form.image.files[0]
+    // const formData = new FormData()
+    // formData.append('image', image)
+
+    const photoURL = await imageUpload(image)
+
+    // console.log(image);
+
+    //send image data to imgbb
+    // const {data} = await axios.post(`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`, formData)
+    // return console.log(data);
+
+    // const image_url = data.data.display_url;
 
     try {
       //2. User Registration
       const result = await createUser(email, password)
 
       //3. Save username & profile photo
-      await updateUserProfile(
-        name,
-        'https://lh3.googleusercontent.com/a/ACg8ocKUMU3XIX-JSUB80Gj_bYIWfYudpibgdwZE1xqmAGxHASgdvCZZ=s96-c'
-      )
+      await updateUserProfile( name, photoURL)
       console.log(result)
 
       navigate('/')
@@ -41,7 +53,8 @@ const SignUp = () => {
       await signInWithGoogle()
 
       navigate('/')
-      toast.success('Signup Successful')
+      // eslint-disable-next-line no-undef
+      toast.success(`Google SignUp Successful ${displayName}`)
     } catch (err) {
       console.log(err)
       toast.error(err?.message)
